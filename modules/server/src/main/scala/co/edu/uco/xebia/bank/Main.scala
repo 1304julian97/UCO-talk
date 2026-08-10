f35a5b395ed3d6e77cc49227c9cd3140b3bcc478
1304julian97 <julian.t97@hotmail.com>
2026-08-09 21:03:44 -0500

    Create server

--- modules/server/src/main/scala/co/edu/uco/xebia/bank/Main.scala
+++ modules/server/src/main/scala/co/edu/uco/xebia/bank/Main.scala
@@ -1,7 +1,28 @@
 package co.edu.uco.xebia.bank
 
 import cats.effect.{IO, IOApp}
+import com.comcast.ip4s.*
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.server.Server
+import co.edu.uco.xebia.bank.routes.{AccountsRoutes, DummyAccountImpl}
 
 object Main extends IOApp.Simple:
+
+  private def server: IO[Server] =
+    val accountsAlgebra = new DummyAccountImpl()
+    val routes = AccountsRoutes.routes(accountsAlgebra)
+
+    EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(port"8080")
+      .withHttpApp(routes.orNotFound)
+      .build
+      .allocated
+      .map(_._1)
+
   override val run: IO[Unit] =
-    IO.println("Hello, world!")
+    server.flatMap { srv =>
+      IO.println(s"Server started at ${srv.address}") >>
+        IO.never
+    }
