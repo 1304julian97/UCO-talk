833c6f126fa6df2a5f0df0656d24d1dbb3d6949d
Luis Miguel Mejía Suárez <luismiguelmejiasuarez@gmail.com>
2026-08-12 07:33:21 -0500

    Add `AccountDoesNotExist` error

--- modules/domain/src/main/scala/co/edu/uco/xebia/bank/payments/models/PaymentError.scala
+++ modules/domain/src/main/scala/co/edu/uco/xebia/bank/payments/models/PaymentError.scala
@@ -10,4 +10,5 @@ enum PaymentError(message: String) extends RuntimeException(message), NoStackTra
       extends PaymentError(s"insufficient funds in account $account")
   case CurrencyMismatch(account: AccountId, expected: Currency, actual: Currency)
       extends PaymentError(s"account $account holds $expected, not $actual")
+  case AccountDoesNotExist(account: AccountId) extends PaymentError(s"account $account does not exist")
   case SameAccountTransfer(account: AccountId) extends PaymentError(s"cannot transfer to the same account $account")
