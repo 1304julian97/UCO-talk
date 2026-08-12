5bbbe219b99d6bff7a24e484fbd156dfcd899b85
Luis Miguel Mejía Suárez <luismiguelmejiasuarez@gmail.com>
2026-08-12 18:10:52 -0500

    Add small API test in the main

--- modules/server/src/main/scala/co/edu/uco/xebia/bank/Main.scala
+++ modules/server/src/main/scala/co/edu/uco/xebia/bank/Main.scala
@@ -1,7 +1,70 @@
 package co.edu.uco.xebia.bank
 
-import cats.effect.{IO, IOApp}
+import cats.effect.IO
+import cats.effect.IOApp
+import co.edu.uco.xebia.bank.accounts.algebras.Accounts
+import co.edu.uco.xebia.bank.accounts.models.*
+import co.edu.uco.xebia.bank.payments.algebras.*
+import co.edu.uco.xebia.bank.payments.models.ConversionPolicy
+import co.edu.uco.xebia.bank.persistence.memory.BlockingInMemoryBankPersistence
+import co.edu.uco.xebia.bank.shared.*
 
 object Main extends IOApp.Simple:
   override val run: IO[Unit] =
-    IO.println("Hello, world!")
+    val resources =
+      for
+        persistence <- BlockingInMemoryBankPersistence.make.toResource
+        converter <- CurrencyConverter.make
+        calculator = MoneyCalculator.make(converter)
+        accounts = Accounts.make(persistence)
+        payments = Payments.make(calculator, persistence)
+      yield (accounts, payments)
+
+    resources.use { case (accounts, payments) =>
+      for
+        luisId <- IO.randomUUID
+        luisAccount <- accounts.open(
+          owner = CustomerId(luisId),
+          currency = Currency.COP,
+          name = AccountName.from("BalmungSan").toOption.get
+        )
+
+        jonathanId <- IO.randomUUID
+        jonathanAccount <- accounts.open(
+          owner = CustomerId(jonathanId),
+          currency = Currency.USD,
+          name = AccountName.from("jdeyrson").toOption.get
+        )
+
+        _ <- payments.deposit(
+          account = luisAccount.id,
+          amount = Money(
+            amount = MoneyAmount(BigDecimal(100000)),
+            currency = Currency.COP
+          ),
+          conversion = ConversionPolicy.Convert
+        )
+
+        _ <- payments.deposit(
+          account = jonathanAccount.id,
+          amount = Money(
+            amount = MoneyAmount(BigDecimal(100)),
+            currency = Currency.USD
+          ),
+          conversion = ConversionPolicy.Convert
+        )
+
+        _ <- payments.transfer(
+          from = luisAccount.id,
+          to = jonathanAccount.id,
+          amount = Money(
+            amount = MoneyAmount(BigDecimal(5)),
+            currency = Currency.EUR
+          ),
+          conversion = ConversionPolicy.Convert
+        )
+
+        _ <- accounts.find(luisAccount.id).flatTap(IO.println)
+        _ <- accounts.find(jonathanAccount.id).flatTap(IO.println)
+      yield ()
+    }
